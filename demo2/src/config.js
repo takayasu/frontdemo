9dbb543a454c4b8e590eaa2cc85b4c234ac49c47
a.takayasu <a.takayasu@gmail.com>
2019-07-30 13:43:56 +0900

    Import

---
+++ demo2/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  Region: 'ap-northeast-1',
+  UserPoolId: 'ap-northeast-1_XXXXXXXXX',
+  ClientId: 'YYYYYYYYYYYYYYYYYYYYYYYYYY',
+  IdentityPoolId: 'ap-northeast-1:XXXXXXXX-YYYY-XXXX-YYYY-XXXXXXXXXXXX'
+}
