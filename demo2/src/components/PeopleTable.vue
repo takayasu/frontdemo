1dc56eef99f1a44c47a3581a8e725cfa741b6eca
a.takayasu <a.takayasu@gmail.com>
2019-07-31 11:04:39 +0900

    Table Add Phase2 End

---
+++ demo2/src/components/PeopleTable.vue
@@ -0,0 +1,54 @@
+<template>
+    <el-table
+            ref="multipleTable"
+            :data="tableData"
+            stripe
+            style="width: 60%; margin: auto"
+            >
+        <el-table-column
+                type="selection"
+                width="55">
+        </el-table-column>
+        <el-table-column
+                property="familyName"
+                label="姓"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                property="firstName"
+                label="名"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                property="email"
+                label="Eメール"
+                show-overflow-tooltip>
+        </el-table-column>
+    </el-table>
+</template>
+
+<script>
+    export default {
+        name: "PeropleTable",
+        data: function(){
+            return {
+                tableData: [
+                    {
+                        familyName: '○×',
+                        firstName: '■△',
+                        email: 'hogehoge@xxxx'
+                    },
+                    {
+                        familyName: 'データ2-姓',
+                        firstName: 'データ2ｰ名',
+                        email: 'データ2ｰメール'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- demo2/src/views/Home.vue
+++ demo2/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="home">
         <EditForm />
+        <PeopleTable />
     </div>
 </template>
 
 <script>
 
     import EditForm from '@/components/EditForm.vue';
+    import PeopleTable from '@/components/PeopleTable.vue';
 
     export default {
         name: 'home',
         components: {
             EditForm,
+            PeopleTable
         },
     };
 </script>
